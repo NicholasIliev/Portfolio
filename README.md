c07c933943d981bb2b6b0596d3f9a7cbd5099ff7
Nicholas Iliev <nicholas.iliev.2003@gmail.com>
2023-04-11 23:46:42 +0100

    Fixing incorrectly imported library

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio-Website
-This is my programming portfolio website, which I have built using React JS.
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
-
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "./components";
-
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat"></div>
-        <Navbar />
-        <Hero />
-      </div>
-    </BrowserRouter>
+    <div>
+      Portfolio
+    </div>
   )
 }
 
