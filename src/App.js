db9b7237b42a235d217e5bc5efbf47687903e813
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2023-10-17 16:30:57 +0100

    Creating the navbar and inserting my logo.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Pages/Home/Navbar';
 import Home from './Pages/Home/HomeScreen';
 
 function App() {
   return (
-  <div className="App">
-    <Home />
-  </div>
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path ="/" element={<Home />}></Route>
+            <Route path ="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
