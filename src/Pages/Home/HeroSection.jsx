8e3409e8e36b3182d04f5c97ed2a9f8a7a18eb54
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2023-10-18 12:00:22 +0100

    Adding sections to the navbar and styling it.

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,21 +1,28 @@
 export default function HeroSection() {
     return (
+        // Hero Section container with an ID for linking
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
+                    {/* Section title */}
                     <p className="section--title">Hey I'm Nicholas</p>
-                    <h1 className="hero--section--title"><span className="hero--section--title--highlight">Full Stack</span>{" "}
-                    <br />
-                    Developer
+                    <h1 className="hero--section--title">
+                        {/* Highlighted text within the title */}
+                        <span className="hero--section--title--highlight">Full Stack</span>{" "}
+                        <br />
+                        Developer
                     </h1>
+                    {/* Description of the Hero Section */}
                     <p className="hero--section-description">
-                        I'm a 2nd Year Computer Science Student at the University of manchester.
+                        I'm a 2nd Year Computer Science Student at the University of Manchester.
                         <br /> I'm passionate about building applications and learning new technologies.
                     </p>
                 </div>
+                {/* Button for getting in touch */}
                 <button className="btn btn-primary">Get in touch</button>
             </div>
             <div className="hero--section--img">
+                {/* Image in the Hero Section */}
                 <img src="./img/hero_img.png" alt="Hero Section" />
             </div>
         </section>
--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -2,30 +2,37 @@ import { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 
 function Navbar() {
+    // State to manage the navigation menu's active status
     const [navActive, setNavActive] = useState(false);
 
+    // Function to toggle the navigation menu
     const toggleNav = () => {
         setNavActive(!navActive);
     };
 
+    // Function to close the navigation menu
     const closeMenu = () => {
         setNavActive(false);
     };
 
+    // Use useEffect to handle window resize events
     useEffect(() => {
         const handleResize = () => {
             if (window.innerWidth <= 500) {
-                closeMenu();
+                closeMenu(); // Close the menu when the window is narrow
             }
         };
 
+        // Add a listener for the resize event
         window.addEventListener('resize', handleResize);
 
+        // Clean up the listener when the component unmounts
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
 
+    // Use useEffect to close the menu when the window is narrow initially
     useEffect(() => {
         if (window.innerWidth <= 1200) {
             closeMenu();
@@ -35,8 +42,71 @@ function Navbar() {
     return (
         <nav className={`navbar ${navActive ? "active" : ""}`}>
             <div>
-                <img src="./img/Logo.svg" alt="Logo"/>
+                <img src="./img/logo.svg" alt="Logo" />
             </div>
+            <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+            </a>
+            <div className={`navbar--items ${navActive ? "active" : ""}`}>
+                <ul>
+                    <li>
+                        {/* Links to different sections with smooth scrolling */}
+                        <Link
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="heroSection"
+                            className="navbar--content"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="MyPortfolio"
+                            className="navbar--content"
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={closeMenu}
+                            activeClass="navbar--active-content"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to="AboutMe"
+                            className="navbar--content"
+                        >
+                            About Me
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <Link onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="Contact"
+                className="btn btn-outline-primary"
+            >
+                Contact Me
+            </Link>
         </nav>
     )
 }
