09d8227adb55e310535f98912e50754824373926
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2024-01-02 19:01:02 +0000

    Adding coding lanaguages.

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -1,4 +1,47 @@
+// MySkills.jsx
+import React from "react";
 import data from "../../data/index.json";
+
+const codingLanguages = [
+  // Foundational Web Technologies
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JavaScript" },
+
+  // Frontend Frameworks/Libraries
+  { name: "React" },
+  { name: "Bootstrap" },
+  { name: "Tailwind CSS" },
+
+  // Programming Languages
+  { name: "Python" },
+  { name: "JavaScript" },
+  { name: "Java" },
+  { name: "C" },
+  { name: "C++" },
+  { name: "Haskell" },
+  { name: "TypeScript" },
+
+  // Version Control
+  { name: "Git" },
+
+  // Databases
+  { name: "MySQL" },
+  { name: "MongoDB" },
+  { name: "Couchbase" },
+
+  // Backend Frameworks
+  { name: "Node.js" },
+  { name: "Express.js" },
+  { name: "Flask" },
+
+  // Integrated Development Environments (IDEs)
+  { name: "Visual Studio Code" },
+  { name: "Eclipse" },
+
+  // Other Technologies
+  { name: "Linux" },
+];
 
 export default function MySkills() {
   return (
@@ -22,6 +65,16 @@ export default function MySkills() {
               </div>
             </div>
           ))}
+        </div>
+        <div className="languages">
+          <h3 className="languages--heading">Technologies</h3>
+          <div className="skills">
+            {codingLanguages.map((language, index) => (
+              <div key={index} className="skills__skill">
+                {language.name}
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </>
