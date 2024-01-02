27fe5be8eec026f22f86f9758a7635968a041279
Nicholas Iliev <nicholas.iliev@student.manchester.ac.uk>
2024-01-02 21:37:00 +0000

    Improving loading speed.

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,4 +1,4 @@
-import FadeIn from "react-fade-in";
+// import FadeIn from "react-fade-in";
 
 export default function HeroSection() {
   return (
@@ -6,7 +6,7 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <FadeIn transitionDuration={2000}>
+          {/* <FadeIn transitionDuration={2000}> */}
             {/* Section title */}
             <p className="section--title">Hey I'm Nicholas</p>
             <br />
@@ -26,10 +26,10 @@ export default function HeroSection() {
               <br /> I'm passionate about building applications and learning new
               technologies.
             </p>
-          </FadeIn>
+          {/* </FadeIn> */}
         </div>
         {/* Link for downloading CV */}
-        <FadeIn transitionDuration={2000}>
+        {/* <FadeIn transitionDuration={2000}> */}
           <a
             href={process.env.PUBLIC_URL + "./CVNicholasIliev.pdf"}
             className="btn btn-primary"
@@ -37,14 +37,14 @@ export default function HeroSection() {
           >
             Download CV
           </a>
-        </FadeIn>
+        {/* </FadeIn> */}
       </div>
-      <FadeIn transitionDuration={2000}>
+      {/* <FadeIn transitionDuration={2000}> */}
         <div className="hero--section--img">
           {/* Image in the Hero Section */}
           <img src="./img/hero_img.png" alt="Hero Section" />
         </div>
-      </FadeIn>
+      {/* </FadeIn> */}
     </section>
   );
 }
